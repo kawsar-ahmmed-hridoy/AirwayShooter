4f590af37448dafe69e9feaac66680274d28f87d
Hazerakteritu <hazerakteritu@gmail.com>
2024-07-10 01:12:25 +0600

    add maingamescreen

--- core/src/com/mygdx/game/MyGame.java
+++ core/src/com/mygdx/game/MyGame.java
@@ -2,10 +2,14 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.ui.HomeScreen;
 
 public class MyGame extends ApplicationAdapter {
 	private HomeScreen display;
+	public static final int Width=480;
+	public static final int Height=720;
+	public SpriteBatch batch;
 
 	public void create(){
 		display = new HomeScreen();
